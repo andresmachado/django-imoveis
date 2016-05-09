85396bba93ce0945a4a0ff323f85df46bd541f06
andresmachado <csantos.machado@gmail.com>
2016-05-09 18:07:34 -0300

    Minor fixes

--- django_imoveis/urls.py
+++ django_imoveis/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     url(r'', include('properties.urls')),
     url(r'^users/', include('users.urls')),
     url(r'^chaining/', include('smart_selects.urls')),
