2b7a4ee949156755850d3a38e036d78678f71d5b
andresmachado <csantos.machado@gmail.com>
2016-05-09 19:36:10 -0300

    Minor fixes

--- django_imoveis/settings.py
+++ django_imoveis/settings.py
@@ -141,9 +141,9 @@ USE_THOUSAND_SEPARATOR = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET']
-AWS_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY']
-AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
