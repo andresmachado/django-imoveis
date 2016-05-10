6341733ded35ec988f6f3f1ce6e00e0ed1169a3f
andresmachado <csantos.machado@gmail.com>
2016-05-10 00:29:11 -0300

    Minor changes

--- properties/urls.py
+++ properties/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     url(r'^imoveis/(?P<pk>[0-9]+)/excluir/$', views.property_delete, name='delete_property'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
